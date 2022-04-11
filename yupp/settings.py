f289507e3312de51e26df3ac3a1698782d640304
Abhishek <53704940+abhi526691@users.noreply.github.com>
2022-04-11 12:30:51 +0545

    Update settings.py

--- yupp/settings.py
+++ yupp/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'yupp.urls'
@@ -145,3 +146,4 @@ STATIC_ROOT = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
