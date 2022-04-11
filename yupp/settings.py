9dbe95d2209c952b90fc815abd8f99d8e36ca16d
abhi526691 <vtu10548@veltechuniv.edu.in>
2022-04-11 13:37:23 +0545

    message

--- yupp/settings.py
+++ yupp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,18 +131,20 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS =(
     os.path.join(BASE_DIR,'static'),
 )
+STATIC_ROOT = "/static/"
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
-    
 
-STATICFILES_DIRS=[STATIC_DIR,]
-STATIC_ROOT = "/static/"
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
